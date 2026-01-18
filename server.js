bff48e5a039d069f06bc008ebf9497608c4c694c
Mohamed Arif <01mohamedarif@gmail.com>
2026-01-18 12:47:05 +0530

    Update app text to version 2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('E-Learning Platform - DevOps Auto Scaling Demo');
+  res.end('E-Learning Platform - DevOps Auto Scaling Demo version 2');
 });
 
 server.listen(3000, () => {
